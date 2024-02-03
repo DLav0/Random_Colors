c7ec24d7a30ed2ecc10ef7882aad26619568aa12
Daniel Lavery <lavery0@yahoo.com>
2024-02-03 17:53:35 -0500

    Intense Colors

--- README.md
+++ README.md
@@ -5,3 +5,15 @@ efficient way to mark up the spans for each letter, or to create all the query S
 
 Not really as exciting as I thought it would be.  Colors are kind of pastel looking.  I wonder if I could redefine the random colors function to 
 make them more intense.  Could add another button to access these more intense colors.  
+
+Saturday, February 3, 2024
+
+Experimented with new randomly generating color functions.  The rainbow() function works pretty well.  Used comment notation to put each span
+on a new line for each letter of "Random Colors" without a space being added in.  A line break in html causes a space.  
+
+My next goal is to be able to add a class to any span or div that will randomly color each letter in the string.  Maybe a good excerice to 
+try and do in React.  Not sure if a React component can be as small as a single digit.  
+
+Add another button to compare rainbow() to getRandomColor().
+
+Maybe a timeout function to automate the color changing.  
--- script.js
+++ script.js
@@ -18,31 +18,80 @@ const thirteen = document.querySelector(".thirteen")
 // Use a class to make all of these document objects?
 
 const spanArray = [one,two,three,four,five,six,seven,eight,nine,ten,eleven,twelve,thirteen]
+const spanArrayOne = [one]
 
 
 colorBtn.addEventListener("click", backColor);
 colorBtn.addEventListener("click", letterColor);
+// colorBtn.addEventListener("click", rainbowLog);
+
 
 function backColor() {
     console.log('You did it!');
-    bodyVar.style.background = getRandomColor();
-    header.style.color = 'green';
+    bodyVar.style.background = intenseColor3();
 }
 
+function letterColor() {
+  for (const letter of spanArray) {
+      letter.style.color = intenseColor3()
+  }    
+}
+
+//Note that this runs intenseColor3 for every iteration of the for loop.
+
 function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
   }
+  return color;
+}
 
-  function letterColor() {
-    for (const letter of spanArray) {
-        letter.style.color = getRandomColor()
-    }    
+function intenseColor() {
+  const colorsArray = ['#FF0000 ','#F99A00 ','#F9ED00','#21FC03 ','#1F04E0 ','#F900ED ']
+  return colorsArray[Math.floor(Math.random()*6)]
+}
+
+function intenseColor2() {
+  var letters = '0123456789ABCDEF';
+  var color = '#F';
+  for (var i = 0; i < 5; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
   }
+  return color;
+}
 
+function intenseColor3() {
+  let r = rainbow(100,Math.floor(Math.random() *1000))
+  //I'm not really clear on what these parameters actually do
+  console.log(r)
+  return r;
+}
 
-console.log('test this')
+function rainbow(numOfSteps, step) {
+  // @param numOfSteps: Total number steps to get color, means total colors
+  // @param step: The step number, means the order of the color
+  // This function generates vibrant, "evenly spaced" colours (i.e. no clustering). This is ideal for creating easily distinguishable vibrant markers in Google Maps and other apps.
+  // Adam Cole, 2011-Sept-14
+  // HSV to RBG adapted from: http://mjijackson.com/2008/02/rgb-to-hsl-and-rgb-to-hsv-color-model-conversion-algorithms-in-javascript
+  var r, g, b;
+  var h = step / numOfSteps;
+  var i = ~~(h * 6);
+  var f = h * 6 - i;
+  var q = 1 - f;
+  switch(i % 6){
+      case 0: r = 1; g = f; b = 0; break;
+      case 1: r = q; g = 1; b = 0; break;
+      case 2: r = 0; g = 1; b = f; break;
+      case 3: r = 0; g = q; b = 1; break;
+      case 4: r = f; g = 0; b = 1; break;
+      case 5: r = 1; g = 0; b = q; break;
+  }
+  var c = "#" + ("00" + (~ ~(r * 255)).toString(16)).slice(-2) + ("00" + (~ ~(g * 255)).toString(16)).slice(-2) + ("00" + (~ ~(b * 255)).toString(16)).slice(-2);
+  return (c);
+}
+
+function rainbowLog () {
+  console.log(rainbow(1000,Math.floor(Math.random() *1000)))
+}
